8d4e8663466b2aca5fb089c43c6c521eb8da475e
Alan Wang <wzh19921016@gmail.com>
2024-12-02 18:34:15 -0800

    progress bar without rounding near 100

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const HEIGHT_MAP = {
+  small: 8,
+  medium: 12,
+  large: 24,
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Container size={size}>
+      <Bar value={value}></Bar>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: 370px;
+  height: ${(props) => HEIGHT_MAP[props.size] + 'px'};
+  padding: ${(props) => props.size === 'large' && '4px'};
+  border-radius: ${(props) => props.size === 'large' ? '8px' : '4px'};
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${(props) => props.value + '%'};
+  border-radius: 4px 0px 0px 4px;
+`;
+
 
 export default ProgressBar;
